9257180bc98997fcb43694233dc824cea986d09a
Joel Bornemann <jlb539@drexel.edu>
2018-01-14 17:29:57 -0500

    Added Logging

--- src/main/java/com/line2linecoatings/api/resources/HelloResource.java
+++ src/main/java/com/line2linecoatings/api/resources/HelloResource.java
@@ -1,5 +1,8 @@
 package com.line2linecoatings.api.resources;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +11,11 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class HelloResource
 {
+    public static final Log log = LogFactory.getLog(HelloResource.class);
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getMessage() {
+        log.info("Hello Logging!");
         return "Hello World!";
     }
 
